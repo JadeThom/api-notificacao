c4690c405be6aa1aa2de9c944fd9b6cbc8382f06
Jade <jadethomaz83@gmail.com>
2026-01-30 15:13:00 -0500

    Organizando rotas de APIs em um prefixo de versão (v1) e ajustando middleware.

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,18 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\NotificacaoController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('notificacoes', NotificacaoController::class);
-    Route::post('notificacoes/{notificacao}/enviar', [NotificacaoController::class, 'enviar']);
-});
+Route::prefix('v1')
+    ->middleware('auth:sanctum')
+    ->group(function () {
 
-Route::apiResource('notificacoes', NotificacaoController::class);
-Route::post('notificacoes/{notificacao}/enviar', [NotificacaoController::class, 'enviar']);
+        Route::apiResource('notificacoes', NotificacaoController::class);
+
+        Route::post(
+            'notificacoes/{notificacao}/enviar',
+            [NotificacaoController::class, 'enviar']
+        );
+    });
 Route::post('login', [AuthController::class, 'login']);
+
+
 
